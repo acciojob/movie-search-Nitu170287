7ad5cfc909f99e8f61118e83008d44e43fafc972
Nitu Dubey <nitutiwari01@gmail.com>
2023-08-24 21:17:30 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [ movies, setMovies] = useState([])
+
+  async function displayMovie() {
+    const response = await fetch(
+      `https://omdbapi.com/?s=${searchInput}&apikey=d98b1a72`
+    );
+    const data = await response.json();
+    console.log(data.Search);
+    setMovies(data.Search)
+    
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input type="text" onChange={(e) => setSearchInput(e.target.value)} />
+      <button onClick={displayMovie}>Search</button>
+      <ul>
+      {movies.map((movie)=><div><li>{movie.Title}</li><img src={movie.Poster}/></div>)}
+      </ul>
+      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
