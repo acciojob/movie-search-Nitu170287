04d32ca2a1e85ec3b28a85e824976eb029bbe588
Nitu Dubey <nitutiwari01@gmail.com>
2023-08-24 21:30:33 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,21 @@ import "./../styles/App.css";
 const App = () => {
   const [searchInput, setSearchInput] = useState("");
   const [ movies, setMovies] = useState([])
+  const [error, setError] = useState("")
 
   async function displayMovie() {
     const response = await fetch(
       `https://omdbapi.com/?s=${searchInput}&apikey=d98b1a72`
     );
     const data = await response.json();
+    if(data.Search){
+      setMovies(data.Search)
+      setError("")
+    }else{
+      setError("Invalid movie name. Please try again.")
+      setMovies([])
+    }
     
-    setMovies(data.Search)
     
   }
 
@@ -21,7 +28,7 @@ const App = () => {
       <input type="text" onChange={(e) => setSearchInput(e.target.value)} />
       <button onClick={displayMovie}>Search</button>
       <ul>
-      {movies.map((movie)=><div><li>{movie.Title}</li><img src={movie.Poster}/></div>)}
+      {error ? <p className="error">{error}</p> : movies.map((movie)=><div><li>{movie.Title}</li><img src={movie.Poster}/></div>)}
       </ul>
       
     </div>
